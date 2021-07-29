e3d02fcc60cdfc261dbef3457954f80bd053f810
Madars Vitolins <madars@mavimax.com>
2021-07-29 09:24:29 +0000

    Fixes on logging

--- tests/run.sh
+++ tests/run.sh
@@ -45,7 +45,15 @@ run_test "01_restin"
 run_test "02_tcpgatesv"
 run_test "03_restout"
 
-grep RESULT test.out
+################################################################################
+# Print results in out file
+# use temp file to avoid plotting to the same file which are are grepping...
+################################################################################
+grep RESULT test.out >test.out.tmp
+cat test.out.tmp
+rm test.out.tmp
+################################################################################ 
+
 echo "*** SUMMARY $M_tests tests executed. $M_ok passes, $M_fail failures"
 
 # wait for tail to pick the change

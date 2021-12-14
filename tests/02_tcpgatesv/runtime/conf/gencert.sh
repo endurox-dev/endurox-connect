b1e53ce92fd4cae332affdaabb2277e7c240584d
Madars Vitolins <madars@mavimax.com>
2021-12-14 22:09:59 +0000

    Fixes for ubuntu 20.04

--- tests/02_tcpgatesv/runtime/conf/gencert.sh
+++ tests/02_tcpgatesv/runtime/conf/gencert.sh
@@ -9,7 +9,7 @@ echo "Host name is [$TEST_HOSTNAME]"
 
 set -x
 
-echo "subjectAltName=DNS:$TEST_HOSTNAME" > subj.ext
+echo "subjectAltName=DNS:$TEST_HOSTNAME" > altsubj.ext
 
 # Generate root CA
 openssl req -nodes -x509 -newkey rsa:2048 -keyout ca.key -out ca.crt -subj "/C=LV/ST=RIGA/L=Riga/O=Endurox_CA/OU=root/CN=$TEST_HOSTNAME/emailAddress=test@mavimax.com"
@@ -18,11 +18,11 @@ openssl req -nodes -x509 -newkey rsa:2048 -keyout ca.key -out ca.crt -subj "/C=L
 openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.csr -subj "/C=LV/ST=RIGA/L=Riga/O=Endurox_SV/OU=root/CN=$TEST_HOSTNAME/emailAddress=test@mavimax.com"
 
 # Sign server cert
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -extfile subj.ext
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -extfile altsubj.ext
 
 # Generate client cert
 openssl req -nodes -newkey rsa:2048 -keyout client.key -out client.csr -subj "/C=LV/ST=RIGA/L=Riga/O=Endurox_CL/OU=root/CN=$TEST_HOSTNAME/emailAddress=test@mavimax.com"
 
 # Sign client cert
-openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAserial ca.srl -out client.crt -extfile subj.ext
+openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAserial ca.srl -out client.crt -extfile altsubj.ext
 
